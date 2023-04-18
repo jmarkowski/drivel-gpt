8a7e9d3abc7f608c92003da8de3d78a767701e69
Jan Markowski <jmarkowski@yelp.com>
2023-04-18 15:05:22 -0400

    Read the input data

--- gpt.py
+++ gpt.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python
+def read_text_data(source):
+    with open(source, 'r', encoding='utf-8') as f:
+        text = f.read()
+
+    print(f'Length of dataset in characters: {len(text)}')
+
+    return text
+
+
 def main():
-    pass
+    input = 'input.txt'
+
+    text = read_text_data(input)
 
 
 if __name__ == '__main__':
