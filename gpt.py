7953601a6c9f21cce156d71753e3d0a0271e1ad8
Jan Markowski <jmarkowski@yelp.com>
2023-04-18 15:37:50 -0400

    Create a tokenizer for encoding/decoding strings/vals

--- gpt.py
+++ gpt.py
@@ -17,12 +17,42 @@ def get_text_stats(text):
     return chars, vocab_size
 
 
+class Tokenizer():
+    """
+    This basic tokenizer is used to support encoding a string of text into an
+    array of integers, and vice versa through decoding.
+    """
+    def __init__(self, char_set):
+        self.chars = char_set
+
+    def encode(self, str):
+        str2int_map = {c:index for index,c in enumerate(self.chars)}
+        encoded_vals = [str2int_map[c] for c in str]
+
+        #print(f'Encoded "{str}" to {encoded_vals}')
+
+        return encoded_vals
+
+    def decode(self, input_vals):
+        int2str_map = {index:c for index,c in enumerate(self.chars)}
+        decoded_str = ''.join([int2str_map[i] for i in input_vals])
+
+        #print(f'Decoded {input_vals} to "{decoded_str}"')
+
+        return decoded_str
+
+
 def main():
     input = 'input.txt'
 
     text = read_text_data(input)
 
     chars, vocab_size = get_text_stats(text)
+
+    # Create tokenizer
+    t = Tokenizer(chars)
+    t.encode('hello world')
+    t.decode(t.encode('hello world'))
 
 
 if __name__ == '__main__':
