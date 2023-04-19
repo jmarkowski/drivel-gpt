f1a8adde3942028cd42871482ba3d3d0988f7a15
Jan Markowski <jmarkowski@yelp.com>
2023-04-19 10:24:20 -0400

    Implement the generate function for the Bigram Language Model

--- gpt/bigram_language_model.py
+++ gpt/bigram_language_model.py
@@ -30,3 +30,33 @@ class BigramLanguageModel(nn.Module):
             loss = F.cross_entropy(logits, targets)
 
         return logits, loss
+
+    def generate(self, idx, max_new_tokens):
+        """
+        This is the generate function for the model.
+
+        idx is (B, T) array of indices in the current context
+
+        The purpose is to take a (B, T), and turn it into a
+        (B, T+1 [+2, +3, ..., +max_new_tokens]) -> This is the generation part.
+        """
+        for _ in range(max_new_tokens):
+            # Get the predictions, but ignore the loss.
+            logits, _loss = self(idx)
+
+            # Focus only on the last time step, we pluck out the last element
+            # in the time direction (because those are the predictions for what
+            # comes next).
+            logits = logits[:, -1, :]                           # becomes (B, C)
+
+            # Apply softmax to get probabilities
+            probs = F.softmax(logits, dim=-1)                   # (B, C)
+
+            # Sample from the distribution (just one sample!) a single prediction
+            # for what comes next!
+            idx_next = torch.multinomial(probs, num_samples=1)  # (B, 1)
+
+            # Append sampled index to the running sequence
+            idx = torch.cat((idx, idx_next), dim=1)             # (B, T+1)
+
+        return idx
